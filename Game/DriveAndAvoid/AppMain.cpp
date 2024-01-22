22372b6168018d40061b64f6b19e2f70502af92b
GimaShinta <shinta-gima-k23f@stu.kbc.ac.jp>
2024-01-22 14:47:18 +0900

    修正
    プロパティの修正

--- Game/DriveAndAvoid/AppMain.cpp
+++ Game/DriveAndAvoid/AppMain.cpp
@@ -5,7 +5,7 @@
 #include "Scene/SceneManager.h"
 
 //メイン関数（プログラムはここから始まります。）
-int WINAPI Winmain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
 	//例外処理（異常が発生したら、catch武文に飛びます）
